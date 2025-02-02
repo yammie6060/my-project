d4555dc9452d1d1f9d6b9ebd5f0ee6065d528777
yammie6060 <yamikanisuwedi6@gmail.com>
2025-02-02 13:57:59 +0200

    changed some other things

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -103,8 +103,6 @@ const Layout = ({ onLogout }) => {
           aria-hidden="true"
         />
       )}
-
-      {/* Sidebar */}
       <aside
         className={`fixed inset-y-0 left-0 z-50 transition-transform duration-300 ease-in-out ${
           isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
@@ -152,8 +150,6 @@ const Layout = ({ onLogout }) => {
           ))}
         </nav>
       </aside>
-
-      {/* Header */}
       <header
         className={`fixed top-0 left-0 right-0 z-50 h-16 flex items-center justify-between px-4 md:px-6 border-2 shadow-lg rounded-r-xl transition-all duration-300  ${
           isDark ? 'bg-gray-900 text-white border-gray-700' : 'bg-white text-gray-800 border-gray-200'
@@ -234,7 +230,6 @@ const Layout = ({ onLogout }) => {
         </div>
       </header>
 
-      {/* Main Content */}
       <div
         className={`flex-1 overflow-auto transition-margin duration-300 ease-in-out ${
           isSidebarOpen ? (isMobile || isIconsOnly ? 'ml-20' : 'ml-64') : 'ml-0'
