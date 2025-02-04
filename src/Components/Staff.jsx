86d0ac9203dbe5cadab7cb7d7a3d2d3eeafad478
yammie6060 <yamikanisuwedi6@gmail.com>
2025-02-04 16:31:11 +0200

    comitting

--- src/Components/Staff.jsx
+++ src/Components/Staff.jsx
@@ -61,7 +61,7 @@ const StaffPage = () => {
               Add Staff
             </Button>
           </div>
-
+        
           {isAddStaffFormOpen && (
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <Modal show={isAddStaffFormOpen} onClose={toggleAddStaffForm} size="md">
